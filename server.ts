fed4973aea60176108f61c061d3081fa1b78dee0
Angelo Santos <axngrsantos@gmail.com>
2023-05-23 11:36:17 +0800

    feat: setup express app

--- server.ts
+++ server.ts
@@ -1,18 +1,30 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
-const port = 8000
+require('dotenv').config();
 
-const app = express();
+const projectRoutes = require('./routes/project')
 
-app.get('/', (req, res) => {
-	res.send('Hello Typescript')
+const app = express()
+
+
+app.use(express.json())
+
+app.use((req, res, next) => {
+	next();
 })
 
-app.get('/hi', (req, res) => {
-	res.send('Ha')
-})
+// routes
+app.use('/api/projects', projectRoutes)
 
 
-app.listen(port,() => {
-	console.log(`listening on port ${port}`)
-})
+// connect to the database
+mongoose.connect(process.env.MONGO_URI!)
+	.then(() => {
+		app.listen(process.env.PORT, () => {
+			console.log(`Connected to the database and listening on port ${process.env.PORT}`)
+		})
+	})
+	.catch((error) => {
+		console.log(error)
+	})
